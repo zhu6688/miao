2a7bfe5d1bce68c3e7b40deaa9a9236aca7ad2bc
yanjie <30750224@qq.com>
2019-04-30 16:42:27 +0800

    404提交

---
+++ src/components/NotFound.vue
@@ -0,0 +1,61 @@
+<template>
+<div class="body">
+    <div id="err404">
+        <img class="png404" src="../../public/img/404.png" />
+        <div class="text">:( 抱歉，您访问的页面不存在！ </div>
+        <p>
+            <router-link to="/" class="go-home">返回首页</router-link> 
+        </p>
+    </div>
+    <div id="cloud"></div>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+.body {
+    width:100%;
+    height: 100%;;
+  background: #67ACE4;
+}
+#err404 {
+  margin: 0 auto;
+  padding-top: 50px;
+  text-align: center;
+}
+#err404 .png404 {
+  margin: 0 auto 50px;
+  width: 192px;
+}
+#err404 .text {
+  color: #fff;
+  font-size: 16px;
+  margin-bottom:20px;
+}
+#err404 .go-home {
+  text-decoration: none;
+  color: #5d4f4f;
+  height: 34px;
+  line-height: 34px;
+  text-align: center;
+  background: #f5f5f5;
+  display: block;
+  width: 100px;
+  border-radius: 20px;
+  margin: 0 auto;
+  font-size:16px;
+}
+#cloud {
+  background: url("../../public/img/error_cloud.png") repeat-x 0 0 transparent;
+  bottom: 0;
+  height: 170px;
+  position: absolute;
+  width: 100%;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,8 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  //base: process.env.BASE_URL,
+  base:'/miao',
   routes: [ // 页面级别的路由，去每个页面中单独在做，页面的路由嵌套
     MovieRouter,
     CnemaRouter,
@@ -15,6 +16,10 @@ export default new Router({
     {
       path:'/',
       redirect:'/movie'
+    },
+    {
+      path:'*',
+      component:()=>import('@/components/NotFound')
     }
   ]
 })
